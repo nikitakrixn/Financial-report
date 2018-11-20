99f0c2cc805fe9eb6732a4a828e6bd596c251a23
Nikita Naidenov <39212305+nikitakrixn@users.noreply.github.com>
2018-11-21 00:50:14 +0600

    Финансовый отчёт

---
+++ Catch_A_Fire/AddForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catch_A_Fire
+{
+    public partial class AddForm : Form
+    {
+        //Перетаскивание формы
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Hide();//Выход
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized; //Свернуть
+        }
+
+        private void pnlTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Перемещение
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
