cc0f3a969c2bdf4886b60d1a356a7d0a18b6dbee
Nikita Naidenov <39212305+nikitakrixn@users.noreply.github.com>
2018-12-17 19:05:59 +0600

    Финансовый отчёт
    Модель базы данных

---
+++ Catch_A_Fire/Enum_users.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Catch_A_Fire
+{
+    internal class Enum_users
+    {
+        public enum Users
+        {
+            Unchanged = 1,
+            Insert,
+            Update,
+            Delete
+        }
+    }
+}
